830f6cf24c51cb339b7ce3c596c9b751ec864d38
Codersheepchen <chenjiaxun2@163.com>
2020-01-12 12:43:47 +0800

    项目迁移到了idea

---
+++ src/main/java/com/chenj/blog/web/IndexController.java
@@ -0,0 +1,23 @@
+package com.chenj.blog.web;
+
+import com.chenj.blog.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+
+
+        return "index";
+    }
+    @GetMapping("/blog")
+    public String blog(){
+
+
+        return "blog";
+    }
+}
